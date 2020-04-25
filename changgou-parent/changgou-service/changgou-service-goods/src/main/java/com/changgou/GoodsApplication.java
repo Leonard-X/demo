45f6e0bcdae9ba1f311bea4a6a3ca3b9b4509e80
leonard <jipeng.zhang@huoquanquan.cn>
2020-04-25 14:50:54 +0800

    电商demo

--- changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
@@ -3,9 +3,11 @@ package com.changgou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableEurekaClient//开启Eureka客户端
+@MapperScan(basePackages = {"com.changgou.dao"})//通用mapper扫描
 public class GoodsApplication {
     public static void main(String[] args) {
         SpringApplication.run(GoodsApplication.class,args);
